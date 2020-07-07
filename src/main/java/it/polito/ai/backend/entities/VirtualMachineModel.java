599eea97adfd4752e2f52ebfe72df29195c8f9bc
Antonio Santoro <a.santoro1993@gmail.com>
2020-07-07 18:52:16 +0200

    vm model resource counters

--- src/main/java/it/polito/ai/backend/entities/VirtualMachineModel.java
+++ src/main/java/it/polito/ai/backend/entities/VirtualMachineModel.java
@@ -14,12 +14,32 @@ public class VirtualMachineModel {
     @Id
     @GeneratedValue
     Long id;
+    /**
+     * min-max resources per vm
+     */
     int min_vcpu;
     int max_vcpu;
     int min_disk;
     int max_disk;
     int min_ram;
     int max_ram;
+
+    /**
+     * tot max resources per vms
+     */
+    int tot_vcpu;
+    int tot_disk;
+    int tot_ram;
+
+    /**
+     * total number of vms both active and inactive
+     */
+    int tot;
+
+    /**
+     * total number of active vms
+     */
+    int max_on;
 
     @OneToOne(mappedBy = "vm_model")
     Course course;
