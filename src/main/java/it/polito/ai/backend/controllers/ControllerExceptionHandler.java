558a9b13542f6c459d69ef22d4964735842cd008
Antonio Santoro <a.santoro1993@gmail.com>
2020-07-26 20:11:40 +0200

    swagger doc setup

--- src/main/java/it/polito/ai/backend/controllers/ControllerExceptionHandler.java
+++ src/main/java/it/polito/ai/backend/controllers/ControllerExceptionHandler.java
@@ -8,28 +8,36 @@ import it.polito.ai.backend.services.vm.VirtualMachineServiceConflictException;
 import it.polito.ai.backend.services.vm.VirtualMachineServiceNotFoundException;
 import org.modelmapper.MappingException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({TeamServiceNotFoundException.class, VirtualMachineServiceNotFoundException.class, AssignmentNotFoundException.class, ExerciseNotFoundException.class})
-    void handleNotFoundException(HttpServletResponse response, Exception exception) throws IOException {
-        response.sendError(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    ResponseEntity<String> handleNotFoundException(HttpServletResponse response, Exception exception, RuntimeException runtimeException) throws IOException {
+        // response.sendError(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+        return new ResponseEntity<>(runtimeException.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({TeamServiceConflictException.class, VirtualMachineServiceConflictException.class})
-    void handleConflictException(HttpServletResponse response, Exception exception) throws IOException {
-        response.sendError(HttpStatus.CONFLICT.value(), exception.getMessage());
+    @ResponseStatus(HttpStatus.CONFLICT)
+    ResponseEntity<String> handleConflictException(HttpServletResponse response, Exception exception, RuntimeException runtimeException) throws IOException {
+        // response.sendError(HttpStatus.CONFLICT.value(), exception.getMessage());
+        return new ResponseEntity<>(runtimeException.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler({IllegalArgumentException.class, ClassCastException.class, MappingException.class/*, NullPointerException.class*/})
-    void handleArgumentException(HttpServletResponse response, Exception exception) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String> handleArgumentException(HttpServletResponse response, Exception exception, RuntimeException runtimeException) throws IOException {
+        // response.sendError(HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(runtimeException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
