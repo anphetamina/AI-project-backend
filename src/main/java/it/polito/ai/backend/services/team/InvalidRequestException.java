5e14ac244c2bee996096f3d5fd31834f9a6de0a8
Antonio Santoro <a.santoro1993@gmail.com>
2020-07-31 22:14:17 +0200

    removed unused exception

--- src/main/java/it/polito/ai/backend/services/team/InvalidRequestException.java
+++
@@ -1,9 +0,0 @@
-package it.polito.ai.backend.services.team;
-
-
-// todo remove
-public class InvalidRequestException extends TeamServiceException {
-    public InvalidRequestException(String message) {
-        super(message);
-    }
-}
