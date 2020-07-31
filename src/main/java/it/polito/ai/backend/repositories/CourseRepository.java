b5ab132d77bfd8d8c5803a815eaefe487e52abcd
Antonio Santoro <a.santoro1993@gmail.com>
2020-07-31 22:14:51 +0200

    fixed custom queries to use enum instead of integer values for checking the status

--- src/main/java/it/polito/ai/backend/repositories/CourseRepository.java
+++ src/main/java/it/polito/ai/backend/repositories/CourseRepository.java
@@ -11,9 +11,9 @@ import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
-    @Query("SELECT s FROM Student s INNER JOIN s.teams t INNER JOIN t.course c WHERE c.id=:courseId AND t.status=1")
+    @Query("SELECT s FROM Student s INNER JOIN s.teams t INNER JOIN t.course c WHERE c.id=:courseId AND t.status=it.polito.ai.backend.entities.TeamStatus.ACTIVE")
     List<Student> getStudentsInTeams(String courseId);
-    @Query("SELECT s FROM Student s INNER JOIN s.courses c WHERE c.id=:courseId AND s NOT IN (SELECT s FROM Student s INNER JOIN s.teams t INNER JOIN t.course c WHERE c.id=:courseId AND t.status=1)")
+    @Query("SELECT s FROM Student s INNER JOIN s.courses c WHERE c.id=:courseId AND s NOT IN (SELECT s FROM Student s INNER JOIN s.teams t INNER JOIN t.course c WHERE c.id=:courseId AND t.status=it.polito.ai.backend.entities.TeamStatus.ACTIVE)")
     // @Query("SELECT s FROM Student s JOIN s.courses c LEFT OUTER JOIN s.teams t ON t.course.id=:courseId WHERE c.id=:courseId AND t.id IS NULL")
     List<Student> getStudentsNotInTeams(String courseId);
 
