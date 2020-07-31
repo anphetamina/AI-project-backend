2f466a7e1b0208aa9664bc77ef7c83ca14be1ad7
taba95 <tabacaru.ralucag@gmail.com>
2020-07-31 13:14:16 +0200

    Add same security

---
+++ src/main/java/it/polito/ai/backend/configuration/SecurityConfig.java
@@ -0,0 +1,40 @@
+package it.polito.ai.backend.configuration;
+
+
+import it.polito.ai.backend.security.JwtSecurityConfigurer;
+import it.polito.ai.backend.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.httpBasic().disable().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/API/**").authenticated()
+                .and()
+                .apply(new JwtSecurityConfigurer(jwtTokenProvider));
+    }
+}
---
+++ src/main/java/it/polito/ai/backend/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package it.polito.ai.backend.repositories;
+
+
+import it.polito.ai.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
