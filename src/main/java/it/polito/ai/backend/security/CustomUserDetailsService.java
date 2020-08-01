2f0edd7ca5b6edb9e33ed95306e47565652e6354
Antonio Santoro <a.santoro1993@gmail.com>
2020-08-01 19:24:21 +0200

    duplicated dependencies

--- src/main/java/it/polito/ai/backend/security/CustomUserDetailsService.java
+++ src/main/java/it/polito/ai/backend/security/CustomUserDetailsService.java
@@ -4,14 +4,16 @@ import it.polito.ai.backend.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
     private UserRepository users;
+
     public CustomUserDetailsService(UserRepository users) {
         this.users = users;
     }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return this.users.findByUsername(username)
